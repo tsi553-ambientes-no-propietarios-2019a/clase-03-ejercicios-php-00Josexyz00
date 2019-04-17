329d0488bedae03a362f38f30dc9c024d19ff12d
00Josexyz00 <jose.cortez@epn.edu.ec>
2019-04-16 19:43:19 -0500

    Ejercicio_2 resuelto

--- ejercicio2/index.php
+++ ejercicio2/index.php
@@ -4,4 +4,31 @@
  *
  * Implementar una función que muestre un título centrado en pantalla, y la llamaremos posteriormente dos veces.
 */
+
+	function frase($fra){
+
+		echo "<h1><Center>$fra</Center></h1>";
+
+	}
 ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ejercicio 2</title>
+</head>
+<body>
+
+	<?php 
+
+		$i=0;
+		do{
+			frase("Hola Mundo");
+			$i+=1;
+		}while($i<2);
+		
+	 ?>
+
+</body>
+</html>
