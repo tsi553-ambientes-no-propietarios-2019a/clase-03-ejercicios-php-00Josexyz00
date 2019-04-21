a617b782a55c749163bc182b9ac21906c667e391
00Josexyz00 <jose.cortez@epn.edu.ec>
2019-04-21 15:48:14 -0500

    Ejercicio 5

--- ejercicio5/index.php
+++ ejercicio5/index.php
@@ -12,4 +12,52 @@ Para imprimir el contenido de una variable también utilizamos el comando echo:
 
 echo $num;
  */
+
+	function aleatoria(){
+
+		$num=rand(1,100);
+		return $num;
+
+	}
+
+	function Mayor_Menor($aleato){
+
+		if ($aleato<50) {
+
+			echo "El número es menor a 50";
+			
+		}elseif ($aleato==50) {
+
+			echo "El número es igual a 50";
+			
+		}else{
+
+			echo "El número es mayor a 50";
+
+		}
+	}
+
+
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title> <center> Ejercicio 5 </center></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+
+	<?php 
+
+		$numAlea=aleatoria();
+		echo "El numero es = ".$numAlea."<br>";
+		Mayor_Menor($numAlea);
+
+
+	 ?>
+	
+</body>
+</html>
